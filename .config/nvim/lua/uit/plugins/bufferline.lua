ac06eca58ca0252a86f072e6a0390ff91f2f5518
kien2346 <91678288+kien2346@users.noreply.github.com>
2023-09-20 19:52:54 +0700

    update nvim-bufferline

--- .config/nvim/lua/uit/plugins/bufferline.lua
+++ .config/nvim/lua/uit/plugins/bufferline.lua
@@ -2,19 +2,26 @@ return {
   "akinsho/bufferline.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   version = "*",
-  opts = {
-  --   options = {
-  --     mode = "tabs",
-  --     separator_style = "slant",
-  --   },
-  },
   config = function()
-    local bufferline = require('bufferline')
-    bufferline.setup({})
+    local bufferline = require("bufferline")
+    bufferline.setup({
+      options = {
+        separator_style = "thick", -- "slant" | "slope" | "thick" | "thin" | { 'any', 'any' }
+        diagnostics = "nvim_lsp",
+        -- offsets = {
+        --   {
+        --     filetype = "NvimTree",
+        --     text = "File Explorer",
+        --     highlight = "Directory",
+        --     -- text_align = "left",
+        --     separator = false, -- use a "true" to enable the default, or set your own character
+        --   },
+        -- },
+      },
+    })
     setKeyMap("n", "<A-l>", ":bnext<CR>")
     setKeyMap("n", "<A-h>", ":bprevious<CR>")
-    setKeyMap("n", "<C-w>", ":bdelete<CR>")
-    
-
+    -- setKeyMap("n", "<C-w>", ":bdelete<CR>")
+    setKeyMap("n", "<C-w>", ":bp<bar>sp<bar>bn<bar>bd<CR>")
   end,
 }
