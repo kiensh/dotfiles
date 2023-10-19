d05d70f1673bf35fefc9e0eddc2d1e0e84d4017c
kien2346 <91678288+kien2346@users.noreply.github.com>
2023-10-19 11:36:09 +0700

    update

--- .config/dunst/sound/playsound.sh
+++ .config/dunst/sound/playsound.sh
@@ -3,8 +3,15 @@
 exclude_list="Spotify test"
 appname=$1
 
-if [[ $exclude_list != *"$appname"* ]]; then
-    # echo "$1"
-    paplay ~/.config/dunst/sound/notification-sound.mp3
-fi
+# if [[ $exclude_list != *"$appname"* ]]; then
+#     echo "$1"
+#     paplay ~/.config/dunst/sound/notification-sound.mp3
+# fi
 
+
+case "$appname" in
+    "Spotify" | "test") :
+    ;;
+    *) paplay ~/.config/dunst/sound/notification-sound.mp3
+    ;;
+esac
