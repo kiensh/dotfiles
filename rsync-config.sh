a08869bb493f24175a2451698ff790bbe21f9617
kien2346 <91678288+kien2346@users.noreply.github.com>
2023-10-20 10:40:09 +0700

    add xorg.conf.d

--- rsync-config.sh
+++ rsync-config.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-rsync -auP --ignore-non-existing ~/.config/ .config
-rsync -auP ~/.config/mpv/ .config/mpv
+rsync -auP --ignore-non-existing /home/uit/.config/ .config
+rsync -auP --ignore-non-existing /etc/X11/xorg.conf.d/ ./xorg.conf.d
+rsync -auP /home/uit/.config/mpv/ .config/mpv
 
