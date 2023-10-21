462d1c56646065ee999f97e4edb867c9da97f6f7
kiensh <91678288+kiensh@users.noreply.github.com>
2023-10-21 15:53:05 +0700

    add copilot

--- rsync-config.sh
+++ rsync-config.sh
@@ -2,5 +2,7 @@
 
 rsync -auP --ignore-non-existing /home/uit/.config/ .config
 rsync -auP --ignore-non-existing /etc/X11/xorg.conf.d/ ./xorg.conf.d
+
 rsync -auP /home/uit/.config/mpv/ .config/mpv
+rsync -auP /home/uit/.config/nvim/ .config/nvim
 
