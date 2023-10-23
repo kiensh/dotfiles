3d51de9147de4a6a51f2a2914638c8cf07f1fbba
kien2346 <91678288+kien2346@users.noreply.github.com>
2023-10-23 21:45:38 +0700

    add Xresources, ideavimrc and update rsync-config

--- rsync-config.sh
+++ rsync-config.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-rsync -auP --ignore-non-existing /home/uit/.config/ .config
+rsync -auP --ignore-non-existing /home/uit/.config/ config
 rsync -auP --ignore-non-existing /etc/X11/xorg.conf.d/ ./xorg.conf.d
 
-rsync -auP /home/uit/.config/mpv/ .config/mpv
-rsync -auP /home/uit/.config/nvim/ .config/nvim
+rsync -auP /home/uit/.config/mpv/ config/mpv
+rsync -auP /home/uit/.config/nvim/ config/nvim
+rsync -auP /home/uit/.ideavimrc .ideavimrc
 
