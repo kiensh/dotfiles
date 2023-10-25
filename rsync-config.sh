001e2464ca18ebb731e7af715e9e767898528bce
kien2346 <91678288+kien2346@users.noreply.github.com>
2023-10-25 16:11:13 +0700

    update config

--- rsync-config.sh
+++ rsync-config.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 rsync -auP --ignore-non-existing /home/uit/.config/ config
-rsync -auP --ignore-non-existing /etc/X11/xorg.conf.d/ ./xorg.conf.d
 
 rsync -auP /home/uit/.config/mpv/ config/mpv
-rsync -auP /home/uit/.config/nvim/ config/nvim
+# rsync -auP /home/uit/.config/nvim/ config/nvim
 rsync -auP /home/uit/.ideavimrc .ideavimrc
+rsync -auP /etc/udev/rules.d/ etc/udev/rules.d
+rsync -auP /etc/X11/xorg.conf.d/ etc/X11/xorg.conf.d
 
