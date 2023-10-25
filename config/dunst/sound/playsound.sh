862c9095c2efecd9772a7a417a869b3876861a98
kiensh <91678288+kiensh@users.noreply.github.com>
2023-10-25 18:43:00 +0700

    update dunst sound

--- config/dunst/sound/playsound.sh
+++ config/dunst/sound/playsound.sh
@@ -1,17 +1,37 @@
 #!/bin/sh
 
-exclude_list="Spotify test"
+
 appname=$1
+summary=$2
+body=$3
+icon=$4
+urgency=$5
 
-# if [[ $exclude_list != *"$appname"* ]]; then
-#     echo "$1"
-#     paplay ~/.config/dunst/sound/notification-sound.mp3
-# fi
+echo $appname
+echo $summary
+echo $body
+echo $icon
+echo $urgency
 
+exclude_list="Spotify test"
+sound=~/.config/dunst/sound/
+
+case "$urgency" in
+    "CRITICAL") 
+        sound+="critical.mp3"
+        ;;
+    "LOW") 
+        sound+="low.mp3"
+    ;;
+    "NORMAL"|*) 
+        # sound+="normal.mp3"
+        sound+="gawr_gura_a.mp3"
+    ;;
+esac
 
 case "$appname" in
     "Spotify" | "test") :
     ;;
-    *) paplay ~/.config/dunst/sound/notification-sound.mp3
+    *) paplay $sound
     ;;
 esac
--- rsync-config.sh
+++ rsync-config.sh
@@ -3,6 +3,7 @@
 rsync -auP --ignore-non-existing /home/uit/.config/ config
 
 rsync -auP /home/uit/.config/mpv/ config/mpv
+rsync -auP /home/uit/.config/dunst/ config/dunst
 # rsync -auP /home/uit/.config/nvim/ config/nvim
 rsync -auP /home/uit/.ideavimrc .ideavimrc
 rsync -auP /etc/udev/rules.d/ etc/udev/rules.d
