7a736b8fa35ef5cbf09e9ab9589e7d33c6e9c9ca
kiensh <91678288+kiensh@users.noreply.github.com>
2023-11-27 10:21:45 +0700

    update .ideavimrc and add .zprofile

--- rsync-config.sh
+++ rsync-config.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+# .config
 rsync -auP --ignore-non-existing --exclude nvim /home/uit/.config/ config
-#rsync -auP --ignore-non-existing /home/uit/.config/ config
-
 rsync -auP /home/uit/.config/mpv/ config/mpv
 rsync -auP /home/uit/.config/dunst/ config/dunst
 # rsync -auP /home/uit/.config/nvim/ config/nvim
+
+# /home/uit/
 rsync -auP /home/uit/.ideavimrc .ideavimrc
+rsync -auP /home/uit/.Xresources .Xresources
+rsync -auP /home/uit/.zprofile .zprofile
+
+# /etc/
 rsync -auP /etc/udev/rules.d/ etc/udev/rules.d
 rsync -auP /etc/X11/xorg.conf.d/ etc/X11/xorg.conf.d
 
