99cba61f2a73a3d6d890973d4137f32e94c1f230
kiensh <91678288+kiensh@users.noreply.github.com>
2024-01-02 19:04:44 +0700

    remove etc config and update rsync-config

--- rsync-config.sh
+++ rsync-config.sh
@@ -4,12 +4,7 @@
 rsync -auP --ignore-non-existing /Users/kien/.config/ config
 rsync -auP /Users/kien/.config/mpv/ config/mpv
 
-# /home/uit/
-rsync -auP /home/uit/.ideavimrc .ideavimrc
-rsync -auP /home/uit/.Xresources .Xresources
-rsync -auP /home/uit/.zprofile .zprofile
-
-# /etc/
-rsync -auP /etc/udev/rules.d/ etc/udev/rules.d
-rsync -auP /etc/X11/xorg.conf.d/ etc/X11/xorg.conf.d
+# /Users/kien/
+rsync -auP /Users/kien/.ideavimrc .ideavimrc
+rsync -auP /Users/kien/.zprofile .zprofile
 
