62eb49e2946337549db003d093d717d71508c658
kiensh <91678288+kiensh@users.noreply.github.com>
2024-02-01 20:15:50 +0700

    change zprofile to profile

--- rsync-config.sh
+++ rsync-config.sh
@@ -5,5 +5,5 @@ rsync -auP --ignore-non-existing /Users/kien/.config/ config
 
 # /Users/kien/
 rsync -auP /Users/kien/.ideavimrc .ideavimrc
-rsync -auP /Users/kien/.zprofile .zprofile
+rsync -auP /Users/kien/.profile .profile
 
