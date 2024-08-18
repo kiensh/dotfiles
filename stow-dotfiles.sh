33ea3bcc40a18d2a1f3e0a26cdc17c8541ce063b
kien2346 <91678288+kien2346@users.noreply.github.com>
2024-08-18 20:00:00 +0700

    backup: 2024-08-18 20:00:00   - config/lf/lfrc   - home/.ideavimrc   - stow-dotfiles.sh

--- stow-dotfiles.sh
+++ stow-dotfiles.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-stow -v --adopt --target $HOME home
 mkdir -p $HOME/.config
-stow -v --adopt --target $HOME/.config config
 mkdir -p $HOME/scripts
-stow -v --adopt --target $HOME/scripts scripts
 mkdir -p $HOME/backup
+
+stow -v --adopt --target $HOME home
+stow -v --adopt --target $HOME/.config config
+stow -v --adopt --target $HOME/scripts scripts
 stow -v --adopt --target $HOME/backup backup
