a921697f9fe4b9b007b77b80983349d04be3d69b
kien2346 <91678288+kien2346@users.noreply.github.com>
2024-09-25 21:49:09 +0700

    feat: change indent size to 4 spaces

--- scripts/git.zsh
+++ scripts/git.zsh
@@ -21,29 +21,29 @@ function ggp() {
     git push origin ${1-$(git_current_branch)} ${@:2}
 }
 function git_current_branch () {
-        local ref
-        ref=$(git symbolic-ref --quiet HEAD 2> /dev/null) 
-        local ret=$? 
-        if [[ $ret != 0 ]]
-        then
-                [[ $ret == 128 ]] && return
-                ref=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)  || return
-        fi
-        echo ${ref#refs/heads/}
+    local ref
+    ref=$(git symbolic-ref --quiet HEAD 2> /dev/null) 
+    local ret=$? 
+    if [[ $ret != 0 ]]
+    then
+        [[ $ret == 128 ]] && return
+        ref=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)  || return
+    fi
+    echo ${ref#refs/heads/}
 }
 function git_main_branch () {
-        command git rev-parse --git-dir &> /dev/null || return
-        local ref
-        for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk,mainline,default,stable,master}
-        do
-                if command git show-ref -q --verify $ref
-                then
-                        echo ${ref:t}
-                        return 0
-                fi
-        done
-        echo master
-        return 1
+    command git rev-parse --git-dir &> /dev/null || return
+    local ref
+    for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk,mainline,default,stable,master}
+    do
+        if command git show-ref -q --verify $ref
+        then
+            echo ${ref:t}
+            return 0
+        fi
+    done
+    echo master
+    return 1
 }
 function glg() {
     git log --graph --pretty="%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset" --stat
