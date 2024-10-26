52c5e2f51206b41a0caab48d0f439c753cf9e2fa
kiensh <91678288+kiensh@users.noreply.github.com>
2024-10-26 23:19:10 +0700

    feat: add yazi config files

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -17,3 +17,12 @@ alias lA='ls -lA | grep "^d" | grep -v "total" && ls -la | grep -v "^d"'
 
 alias v="nvim"
 
+# yazi
+function y() {
+    local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+    yazi "$@" --cwd-file="$tmp"
+    if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+        builtin cd -- "$cwd"
+    fi
+    rm -f -- "$tmp"
+}
