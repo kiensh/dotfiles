f3f3621a57a6475d04d1e56c7b3b39e5848bfb2e
kien2346 <91678288+kien2346@users.noreply.github.com>
2024-10-27 08:25:12 +0700

    feat: move from ls to eza

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -1,4 +1,7 @@
 
+alias mpv-playlist="ls *.{mp4,mkv} | sort -V | mpv --playlist=-"
+alias v="nvim"
+
 # LINUX
 export LS_COLORS="di=1;36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
 export LS_COLORS="di=1;34:ln=1;36:so=1;35:pi=1;33:ex=1;32:bd=1;33:cd=1;33:su=37;41:sg=30;43:tw=31:ow=31"
@@ -14,8 +17,13 @@ alias lh='ls -ld .?*'
 alias lA='ls -lA | grep "^d" | grep -v "total" && ls -la | grep -v "^d"'
 # alias ll='ls -l'
 # alias lt='ls -ltr'
-
-alias v="nvim"
+if [ "$(command -v eza)" ]; then
+    alias ld='eza -lD'
+    alias lf='eza -lf --color=always | grep -v /'
+    alias lh='eza -dl .* --group-directories-first'
+    alias lA='eza -la --group-directories-first'
+    alias mpv-playlist="eza --no-quotes *.m* | mpv --playlist=-"
+fi
 
 # yazi
 function y() {
