e26056fcc0aaefbf382197fc5eb89b309104619e
kiensh <91678288+kiensh@users.noreply.github.com>
2024-10-31 20:00:00 +0700

    backup: 2024-10-31 20:00:00   - config/tmux/tmux.conf   - scripts/completion.zsh   - scripts/functions.sh

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,4 +1,10 @@
 
+# Test Your Shell Load Time
+function timezsh() {
+  shell=${1-$SHELL}
+  for i in $(seq 1 10); do /usr/bin/time $shell -i -c exit; done
+}
+
 # yazi
 function y() {
     local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
