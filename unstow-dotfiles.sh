1f9c2be098661bbe43f10ac473936a0d79232528
kien2346 <91678288+kien2346@users.noreply.github.com>
2025-02-22 15:16:20 +0700

    feat: update unstow for bin folder

--- unstow-dotfiles.sh
+++ unstow-dotfiles.sh
@@ -4,3 +4,4 @@ stow -v -D --target $HOME home
 stow -v -D --target $HOME/.config config
 stow -v -D --target $HOME/scripts scripts
 stow -v -D --target $HOME/backup backup
+stow -v -D --target $HOME/.local/bin bin
