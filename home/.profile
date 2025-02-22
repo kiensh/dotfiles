7ef79009e3888ed00239480728491f89b5a9b526
kiensh <91678288+kiensh@users.noreply.github.com>
2025-02-22 20:42:09 +0700

    feat: move export Color from alias.sh to .profile

--- home/.profile
+++ home/.profile
@@ -5,6 +5,13 @@ export ZDOTDIR="$HOME"
 export ZSH="$HOME/.zsh"
 mkdir -p "$ZSH"
 
+# Color for Linux
+export LS_COLORS="di=1;36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
+export LS_COLORS="di=1;34:ln=1;36:so=1;35:pi=1;33:ex=1;32:bd=1;33:cd=1;33:su=37;41:sg=30;43:tw=31:ow=31"
+# Color for BSD (Mac OS X)
+export LSCOLORS="Gxfxcxdxbxegedabagacad"
+export LSCOLORS="ExGxFxDxCxDxDxhbadbxbx"
+
 [ -d "$HOME/.local/bin" ] && export PATH+=":$HOME/.local/bin"
 
 # dotnet
--- scripts/alias.sh
+++ scripts/alias.sh
@@ -2,13 +2,6 @@
 alias mpv-playlist="ls *.{mp4,mkv} | sort -V | mpv --playlist=-"
 alias v="vim"
 
-# LINUX
-export LS_COLORS="di=1;36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
-export LS_COLORS="di=1;34:ln=1;36:so=1;35:pi=1;33:ex=1;32:bd=1;33:cd=1;33:su=37;41:sg=30;43:tw=31:ow=31"
-# BSD (Mac OS X)
-export LSCOLORS="Gxfxcxdxbxegedabagacad"
-export LSCOLORS="ExGxFxDxCxDxDxhbadbxbx"
-
 alias grep='grep --color=auto'
 alias ls='ls --color=always'
 alias ld='ls -l | grep "^d" | grep -v "total"'
