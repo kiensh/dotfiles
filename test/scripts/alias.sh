3e402cda46348e407bdac36cf8210aa334692d91
kien2346 <91678288+kien2346@users.noreply.github.com>
2025-03-29 20:00:02 +0700

    backup: 2025-03-29 20:00:02   - bin/7z_folder   - config/yazi/keymap.toml   - test/scripts/alias.sh   - test/scripts/completion.zsh   - test/scripts/functions.sh   - test/scripts/git.sh   - test/scripts/globalias.zsh   - test/scripts/key-bindings.zsh   - test/scripts/plugins.zsh

---
+++ test/scripts/alias.sh
@@ -0,0 +1,29 @@
+
+alias mpv-playlist="ls *.{mp4,mkv} | sort -V | mpv --playlist=-"
+alias v="vim"
+
+alias grep='grep --color=auto'
+alias ls='ls --color=always'
+alias ld='ls -l | grep "^d" | grep -v "total"'
+alias lf='ls -lp --color=always | grep -v "^d" | grep -v "total"'
+alias lh='ls -ld .*'
+alias ll='ls -lA | grep "^d" | grep -v "total" && ls -la | grep -v "^d" | grep -v "total"'
+# alias ll='ls -l'
+# alias lt='ls -ltr'
+if [ "$(command -v eza)" ]; then
+    alias ld='eza -lD --show-symlinks'
+    alias lf='eza -lf --show-symlinks'
+    alias lh='eza -dl .* --group-directories-first'
+    alias ll='eza -la --group-directories-first'
+    alias mpv-playlist="eza --no-quotes *.{mp4,mkv} | mpv --playlist=-"
+fi
+
+# Tools
+[[ $(command -v nvim) ]] && alias v="nvim"
+[[ $(command -v colima) ]] && alias c='colima'
+[[ $(command -v docker) ]] && alias d='docker'
+[[ $(command -v kubectl) ]] && alias k='kubectl'
+[[ $(command -v vifm) ]] && alias vf='vifm .'
+[[ $(command -v python3) ]] && alias py='python3'
+[[ $(command -v pip3) ]] && alias pi='pip3'
+
---
+++ test/scripts/functions.sh
@@ -0,0 +1,19 @@
+
+# Test Your Shell Load Time
+function time-shell() {
+  shell=${1-$SHELL}
+  for i in $(seq 1 10); do /usr/bin/time $shell -i -c exit; done
+}
+
+# yazi
+function y() {
+    # local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+    # yazi "$@" --cwd-file="$tmp"
+    # if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+    #     builtin cd -- "$cwd"
+    # fi
+    # rm -f -- "$tmp"
+    yazi "@"
+    printf "\033[4 q" # change cursor style to underline steady
+}
+
