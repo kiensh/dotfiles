4b044d32e421e5f586a52cd42aad069230e541ed
kien2346 <91678288+kien2346@users.noreply.github.com>
2025-04-07 20:00:00 +0700

    backup: 2025-04-07 20:00:00   - bin/ani   - test.json   - test.sh

--- test.sh
+++ test.sh
@@ -4,8 +4,46 @@ agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefo
 allanime_refr="https://allmanga.to"
 allanime_base="allanime.day"
 allanime_api="https://api.${allanime_base}"
+mode="sub"
 
-episode_link="$(curl -e "$allanime_refr" -s "https://${allanime_base}$*" -A "$agent" | sed 's|},{|\
-|g' | sed -nE 's|.*link":"([^"]*)".*"resolutionStr":"([^"]*)".*|\2 >\1|p;s|.*hls","url":"([^"]*)".*"hardsub_lang":"en-US".*|\1|p')"
+search_anime() {
+    search_gql="
+        query(
+            \$search: SearchInput
+            \$limit: Int
+            \$page: Int
+            \$translationType: VaildTranslationTypeEnumType
+            \$countryOrigin: VaildCountryOriginEnumType) {
+                shows(
+                    search: \$search
+                    limit: \$limit
+                    page: \$page
+                    translationType: \$translationType
+                    countryOrigin: \$countryOrigin) {
+                        edges {
+                            _id
+                            name
+                            availableEpisodes
+                            __typename
+                        }
+                }
+        }"
 
-echo "$episode_link"
+    curl -e "$allanime_refr" -s -G "${allanime_api}/api" -A "$agent" \
+        --data-urlencode "variables={
+            \"search\":{
+                \"allowAdult\":false,
+                \"allowUnknown\":false,
+                \"query\":\"$1\"
+            },
+            \"limit\":40,
+            \"page\":1,
+            \"translationType\":\"$mode\",
+            \"countryOrigin\":\"ALL\"
+        }" \
+        --data-urlencode "query=$search_gql"
+#     | sed 's|Show|\
+# | g' | sed -nE "s|.*_id\":\"([^\"]*)\",\"name\":\"(.+)\",.*${mode}\":([1-9][^,]*).*|\1	\2 (\3 episodes)|p" | sed 's/\\"//g'
+}
+
+search_anime ""
