af7833f2af5e8b771ecc2f06781684d07038cfe6
kien2346 <91678288+kien2346@users.noreply.github.com>
2025-04-11 20:00:00 +0700

    backup: 2025-04-11 20:00:00   - config/kitty/kitty.conf   - config/starship.toml   - config/yazi/init.lua

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -1,70 +1,8 @@
--- remove the status bar
-local function remove_status()
-	local old_layout = Tab.layout
-	Status.render = function()
-		return {}
-	end
-	Tab.layout = function(self, ...)
-		self._area = ui.Rect({ x = self._area.x, y = self._area.y, w = self._area.w, h = self._area.h + 1 })
-		return old_layout(self, ...)
-	end
-end
-remove_status()
+---@ya pack -a yazi-rs/plugins:no-status
+require("no-status"):setup()
 
--- full border
--- TODO: remove this once v0.4 is released
-local function full_border()
-	local v4 = function(typ, area, ...)
-		if typ == "bar" then
-			return ui.Table and ui.Bar(...):area(area) or ui.Bar(area, ...)
-		else
-			return ui.Table and ui.Border(...):area(area) or ui.Border(area, ...)
-		end
-	end
+---@ya pack -a yazi-rs/plugins:full-border
+require("full-border"):setup()
 
-	local type = opts and opts.type or ui.Border.ROUNDED
-	local old_build = Tab.build
-
-	Tab.build = function(self, ...)
-		local bar = function(c, x, y)
-			if x <= 0 or x == self._area.w - 1 then
-				return v4("bar", ui.Rect.default, ui.Bar.TOP)
-			end
-
-			return v4(
-				"bar",
-				ui.Rect({
-					x = x,
-					y = math.max(0, y),
-					w = ya.clamp(0, self._area.w - x, 1),
-					h = math.min(1, self._area.h),
-				}),
-				ui.Bar.TOP
-			):symbol(c)
-		end
-
-		local c = self._chunks
-		self._chunks = {
-			c[1]:padding(ui.Padding.y(1)),
-			c[2]:padding(ui.Padding(c[1].w > 0 and 0 or 1, c[3].w > 0 and 0 or 1, 1, 1)),
-			c[3]:padding(ui.Padding.y(1)),
-		}
-
-        local style = THEME.manager.border_style
-        self._base = ya.list_merge(self._base or {}, {
-            v4("border", self._area, ui.Border.ALL):type(type):style(style),
-            v4("bar", self._chunks[1], ui.Bar.RIGHT):style(style),
-            v4("bar", self._chunks[3], ui.Bar.LEFT):style(style),
-
-			bar("┬", c[1].right - 1, c[1].y),
-			bar("┴", c[1].right - 1, c[1].bottom - 1),
-			bar("┬", c[2].right, c[2].y),
-			bar("┴", c[2].right, c[2].bottom - 1),
-		})
-
-		old_build(self, ...)
-	end
-end
-full_border()
-
+---@ya pack -a dedukun/relative-motions
 require("relative-motions"):setup({ show_numbers = "relative_absolute", show_motion = true })
