3cae3984a665b20e151b1469d11cc8ccc0bda373
kiensh <91678288+kiensh@users.noreply.github.com>
2025-05-20 20:00:00 +0700

    backup: 2025-05-20 20:00:00   - scripts/alias.sh   - scripts/functions.sh

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,23 +1,23 @@
-
 # Test Your Shell Load Time
 function time-shell() {
-  shell=${1-$SHELL}
-  for i in $(seq 1 10); do /usr/bin/time $shell -i -c exit; done
+    shell=${1-$SHELL}
+    for i in $(seq 1 10); do /usr/bin/time $shell -i -c exit; done
 }
 
 # yazi
-function y() {
+function yazi() {
     # local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
     # yazi "$@" --cwd-file="$tmp"
     # if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
     #     builtin cd -- "$cwd"
     # fi
     # rm -f -- "$tmp"
-    yazi "@"
+    /opt/homebrew/bin/yazi "$@"
     printf "\033[4 q" # change cursor style to underline steady
 }
 
 # Typing Test toipe
 function toipe() {
+    /opt/homebrew/bin/toipe "$@"
+    printf "\033[4 q" # change cursor style to underline steady
 }
-
