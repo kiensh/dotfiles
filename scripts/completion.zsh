b7d2c708251b680ea067607f4bb277ae74f60352
kiensh <91678288+kiensh@users.noreply.github.com>
2025-11-19 20:00:01 +0700

    backup: 2025-11-19 20:00:00   - scripts/completion.zsh

--- scripts/completion.zsh
+++ scripts/completion.zsh
@@ -1,10 +1,11 @@
-### Load fpath ###
-# autoload -Uz compinit
-# compinit -u
+autoload -Uz compinit
+compinit -u
 
-### Using Compdef ###
 # function _fzf() { source <(fzf --zsh); }
 # [[ $(command -v fzf) ]] && compdef _fzf fzf
 #
 # function _ng() { source <(ng completion script); }
 # [[ $(command -v ng) ]] && compdef _ng ng
+
+function _npm() { source <(npm completion); }
+[[ $(command -v npm) ]] && compdef _npm npm
