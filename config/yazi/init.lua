63f791fde30995e88c8231ec6b6189530a393559
kiensh <91678288+kiensh@users.noreply.github.com>
2026-02-14 01:52:50 +0700

    feat: update Yazi configuration files

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -5,4 +5,7 @@ require("no-status"):setup()
 require("full-border"):setup()
 
 ---@dedukun/relative-motions
-require("relative-motions"):setup({ show_numbers = "relative_absolute", show_motion = true })
+require("relative-motions"):setup({
+	show_numbers = "relative_absolute",
+	show_motion = true,
+})
