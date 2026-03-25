fe41f4505c82e13bff674cad87c47858b0d29c31
kiensh <91678288+kiensh@users.noreply.github.com>
2026-03-25 20:00:01 +0700

    backup: 2026-03-25 20:00:00   - config/yazi/package.toml   - scripts/alias.sh

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -27,4 +27,4 @@ fi
 [[ $(command -v webtorrent) ]] && alias tor=torrent torrent='webtorrent --not-on-top --mpv $(pbpaste)'
 [[ $(command -v yazi) ]] && alias y=yazi
 [[ $(command -v toipe) ]] && alias t='toipe --wordlist top2500'
-[[ $(command -v cursor) ]] && alias cur='cursor agent' ag=agent
+[[ $(command -v cursor) ]] && alias cur='cursor' ag=agent
